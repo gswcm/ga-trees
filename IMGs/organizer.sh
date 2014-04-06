e190606823eda2e0388e083017ffa57742abb4bc
simonbaev <simonbaev@gmail.com>
2014-04-05 23:53:15 -0400

    Images added

---
+++ IMGs/organizer.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]  
+then
+	echo "Usage: $(basename $0) <file.csv>" 1>&2
+	exit 1
+fi
+
+dirName="organized"
+rm -rf "$dirName" 
+
+[ -f "$1" ] && cat $1 | tail -n +2 | awk -F\" '{print $1 tolower($3)}' | awk -F, '{printf "%s,%s,%s\n",$1,$3,$7}' | sort -t, -k2 | sed 's|/|-|g' | { 
+	IFS=","; 
+	while read a b c; 
+	do 
+		[ -z "$a" -o -z "$b" -o -z "$c" ] && continue;
+		[ -d "$dirName/$b" ] || mkdir -p "$dirName/$b"; 
+		[ -f "$a.jpg" ] && { 
+			cd "$dirName/$b"; 
+			if [ -f "${c}-00.jpg" ]
+			then
+				i=1
+			   while true	
+				do
+					fname="$(printf "%s-%02d" "$c" $i).jpg"
+					if [ ! -f "$fname" ]
+					then
+						break;
+					fi
+					i=$(expr $i + 1)
+				done
+				ln -s "../../$a.jpg" "./$fname";
+			else
+				ln -s "../../$a.jpg" "./$c-00.jpg"; 
+			fi
+			cd ../..; 
+		}; 
+	done; 
+}
+
