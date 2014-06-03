56b9851eea65e60a1addaaff87853c0a38328e4d
simonbaev <simonbaev@gmail.com>
2014-06-02 23:29:33 -0400

    Winter & Summer Key identification added to DB. To populate DB (Misc/ga-trees.db) run "Misc/Winter_Key/WK.sh" (it will rewrite all identification data based on "Misc/Summer_Key/Summer Key.txt" and "Misc/Winter_Key/Winter Key.txt"

--- Misc/Summer_Key/SK.sh
+++ Misc/Summer_Key/SK.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-dbname="ga-trees.db"
+dbname="../ga-trees.db"
+keyname="Summer Key.txt"
 
 add_range() {
 	if [ -z "$1" ] ; then
@@ -14,6 +15,8 @@ add_range() {
 }
 
 sqlite3 "$dbname" "drop table if exists quest_navigation; drop table if exists quest_data; drop table if exists tree_id_range; create table tree_id_range (range_id integer primary key autoincrement, low integer, high integer); CREATE TABLE quest_navigation (quest_id integer, y integer, n integer, y_range_id integer, n_range_id integer, foreign key (y_range_id) references tree_id_range (range_id), foreign key (n_range_id) references tree_id_range (range_id)); CREATE TABLE quest_data (quest_id integer primary key, quest_text text, pic_y text, pic_n text);"
+
+sed '/^\s*$/d' "$keyname" | tr -cd '[a-z][A-Z][0-9] ?.\n-' | tr -s ' ' | sed 's/^ //' | sed '/[0-9][0-9]*/!d' > SK
 
 i=0
 while read line; do
@@ -47,9 +50,9 @@ while read line; do
 				"10") sqlite3 "$dbname" "insert into quest_navigation (quest_id, y, n, y_range_id, n_range_id) values ('$qid', '$yes_next_id', null, null, '$no_range_id');";;
 				"11") sqlite3 "$dbname" "insert into quest_navigation (quest_id, y, n, y_range_id, n_range_id) values ('$qid', '$yes_next_id', '$no_next_id', null, null);";;
 			esac
-			#echo "'$qid' '$qtext' '$yes_next_id' '$no_next_id' '$yes_tree_id' '$no_tree_id'"
+			#echo "'$qid' '$qtext' '$yes_next_id' '$no_next_id' '$yes_tree_id' '$no_tree_id' '$yes_range_id' '$no_range_id'"
 			sqlite3 "$dbname" "insert into quest_data (quest_id, quest_text) values ('$qid', '$qtext');"
 			;;
 	esac
 	((i++))
-done < T1
+done < SK
